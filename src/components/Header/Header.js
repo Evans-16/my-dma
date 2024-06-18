94b75c489c293c271216d42cb1be7e81376f62a9
Evansmosoti <evansmosoti16@gmail.com>
2024-06-18 11:20:39 -0700

    href attribute in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,11 +26,11 @@ function Header() {
           </button>
           <nav className={`nav ${menuOpen ? 'open' : ''}`}>
             <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#team">About</a></li>
-              <li><a href="#services">Services</a></li>
-              <li><a href="#clients">Clients</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <li><a href="/home">Home</a></li>
+              <li><a href="/team">About</a></li>
+              <li><a href="/services">Services</a></li>
+              <li><a href="/clients">Clients</a></li>
+              <li><a href="/contact">Contact</a></li>
               <li><Link to="/FAQs">FAQs</Link></li>
             </ul>
           </nav>
