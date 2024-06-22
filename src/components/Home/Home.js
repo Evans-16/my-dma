e82d64df6eda3d8ccc232247113fafc36cc27875
Evansmosoti <evansmosoti16@gmail.com>
2024-06-22 10:21:49 -0700

    image change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,6 @@ const Home = () => {
           <span className="line">Hi! welcome to Digital Media Africa Agency. We are</span>
           <span className="line">an optimal perfomance striving agency</span>
           <span className="line">through data driven strategies</span>
-          <span className="line">Join Us</span>
         </h2>
         <button className="work-with-us-button" onClick={handleClick}>
         Work with Us
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,23 +4,23 @@ import './Statistics.css';
 const Statistics = () => {
   return (
     <section className="statistics">
-      <h2>We've been helping high-growth tech companies since 2017</h2>
+      <h2>We've been helping high-growth enterprises in Africa since 2015</h2>
       <div className="stats-container">
         <div className="stat">
-          <h3>50+</h3>
+          <h3>100+</h3>
           <p>B2B SAAS CLIENTS WORKED WITH</p>
         </div>
         <div className="stat">
-          <h3>20+</h3>
+          <h3>50+</h3>
           <p>BRAND & PRODUCT LAUNCHES</p>
         </div>
         <div className="stat">
-          <h3>9+</h3>
+          <h3>50+</h3>
           <p>PUBLICLY TRADED ENTERPRISES</p>
         </div>
         <div className="stat">
-          <h3>$133B</h3>
-          <p>TOTAL ESTIMATED CLIENT VALUATION</p>
+          <h3>$200M+</h3>
+          <p>TOTAL ESTIMATED MANAGED BUDGETS</p>
         </div>
       </div>
     </section>
